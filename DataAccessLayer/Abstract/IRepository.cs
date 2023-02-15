9d5de9dad9e9ceeb2f54d559df71e359bfa7143d
mlhcakmak <melihcakmak671@gmail.com>
2023-02-15 22:34:00 +0300

    businesslayer services
    businesslayer katmanında serviceler oluşturuldu.

--- DataAccessLayer/Abstract/IRepository.cs
+++ DataAccessLayer/Abstract/IRepository.cs
@@ -9,10 +9,17 @@ namespace DataAccessLayer.Abstract
 {
     public interface IRepository<T>
     {
+
         List<T> List();
+
         void Insert(T p);
+
         void Update(T p);
+
+        T Get(Expression<Func<T, bool>> filter);
+
         void Delete(T p);
+
         List<T> List(Expression<Func<T, bool>> filter);
     }
 }
