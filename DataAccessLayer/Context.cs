92e63ac1753fff49be8aa0d746b6b34978cfe700
mlhcakmak <melihcakmak671@gmail.com>
2023-02-23 21:17:58 +0300

    small changes
    automatic migration enabled yapılıp context.cs e entityler eklendi.

--- DataAccessLayer/Context.cs
+++ DataAccessLayer/Context.cs
@@ -11,13 +11,13 @@ namespace DataAccessLayer
     public class Context : DbContext
     {
         public DbSet<About> Abouts { get; set; }
-
         public DbSet<Category> Categories { get; set; }
         public DbSet<Contact> Contacts { get; set; }
-
         public DbSet<Content> Contents { get; set; }
-
         public DbSet<Heading> Headings { get; set; }
         public DbSet<Writer> Writers { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<ImageFile> ImageFiles { get; set; }
+        public DbSet<Admin> Admins { get; set; }
     }
 }
